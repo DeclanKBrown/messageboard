ada31432c88b8c09bebbcaedaf5b85e74ed95d50
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-10-08 14:55:46 +1100

    Added form

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New Message' });
+});
+
+router.post('/new', (req, res) => {
+  messages.push({text: req.body.message, user: req.body.name, added: new Date()});
+  res.redirect('/')
+})
+
 module.exports = router;
